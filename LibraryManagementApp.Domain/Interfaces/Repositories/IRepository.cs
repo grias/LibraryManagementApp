41d513c5828f8c484d2b647af3ac6e547d8a5683
Alexander Gribowski <grias1994@gmail.com>
2025-10-18 23:09:24 +0300

    Changed IRepository to explicitly show nullable returned values

--- LibraryManagementApp.Domain/Interfaces/Repositories/IRepository.cs
+++ LibraryManagementApp.Domain/Interfaces/Repositories/IRepository.cs
@@ -4,11 +4,11 @@ public interface IRepository<TEntity> where TEntity : class
 {
     Task<List<TEntity>> GetAllAsync();
 
-    Task<TEntity> GetByIdAsync(int id);
+    Task<TEntity?> GetByIdAsync(int id);
 
     Task<TEntity> CreateAsync(TEntity entity);
 
-    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity?> UpdateAsync(TEntity entity);
 
     Task<TEntity> DeleteAsync(int id);
 }
