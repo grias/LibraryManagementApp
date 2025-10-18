5e038e2b34d0ed0816110907757ab70fbafa5e8e
Alexander Gribowski <grias1994@gmail.com>
2025-10-18 22:05:24 +0300

    Add extension methods for Author model and DTOs

---
+++ LibraryManagementApp.Domain/Mappers/AuthorsMappers.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagementApp.Domain.Dtos.AuthorDtos;
+using LibraryManagementApp.Domain.Dtos.BookDtos;
+using LibraryManagementApp.Domain.Models;
+
+namespace LibraryManagementApp.Domain.Mappers;
+
+public static class AuthorsMappers
+{
+    public static AuthorDto ToAuthorDto(this Author authorModel)
+    {
+        return new AuthorDto()
+        {
+            Id = authorModel.Id,
+            Name = authorModel.Name,
+            DateOfBirth = authorModel.DateOfBirth,
+        };
+    }
+
+    public static Author ToAuthorModel(this CreateAuthorDto authorDto)
+    {
+        return new Author()
+        {
+            Name = authorDto.Name,
+            DateOfBirth = authorDto.DateOfBirth
+        };
+    }
+}
