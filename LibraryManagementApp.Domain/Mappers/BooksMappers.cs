9b5afa7ea3c4ecda3b6e6aedfd63b895f2132766
Alexander Gribowski <grias1994@gmail.com>
2025-10-18 20:50:49 +0300

    Added BookMappers extencion methods

---
+++ LibraryManagementApp.Domain/Mappers/BooksMappers.cs
@@ -0,0 +1,42 @@
+﻿using LibraryManagementApp.Domain.Dtos.BookDtos;
+using LibraryManagementApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementApp.Domain.Mappers
+{
+    public static class BooksMappers
+    {
+        public static BookDto ToBookDto(this Book bookModel)
+        {
+            return new BookDto()
+            {
+                Id = bookModel.Id,
+                Title = bookModel.Title,
+                PublishedYear = bookModel.PublishedYear,
+                AuthorId = bookModel.AuthorId
+            };
+        }
+
+        public static Book ToBookModel(this CreateBookDto bookDto)
+        {
+            return new Book()
+            {
+                Title = bookDto.Title,
+                PublishedYear = bookDto.PublishedYear,
+                AuthorId = bookDto.AuthorId
+            };
+        }
+
+        public static Book ToBookModel(this UpdateBookDto bookDto)
+        {
+            return new Book()
+            {
+                Title = bookDto.Title,
+                PublishedYear = bookDto.PublishedYear,
+                AuthorId = bookDto.AuthorId
+            };
+        }
+    }
+}
