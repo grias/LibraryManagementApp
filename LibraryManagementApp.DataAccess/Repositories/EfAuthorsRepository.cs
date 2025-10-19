4a91fa5ce3d1e6fdd8a3d065d3a273b5b4e1674e
Alexander Gribowski <grias1994@gmail.com>
2025-10-19 17:49:20 +0300

    Implemented Repositories for ef core

---
+++ LibraryManagementApp.DataAccess/Repositories/EfAuthorsRepository.cs
@@ -0,0 +1,62 @@
+﻿using LibraryManagementApp.DataAccess.Contexts;
+using LibraryManagementApp.Domain.Interfaces.Repositories;
+using LibraryManagementApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementApp.DataAccess.Repositories;
+
+public class EfAuthorsRepository : IAuthorsRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public EfAuthorsRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Author>> GetAllAsync()
+    {
+        return await _context.Authors.ToListAsync();
+    }
+
+    public async Task<Author?> GetByIdAsync(int id)
+    {
+        return await _context.Authors.FindAsync(id);
+    }
+
+    public async Task<Author> CreateAsync(Author entity)
+    {
+        await _context.Authors.AddAsync(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+    public async Task<Author?> UpdateAsync(Author entity)
+    {
+        var authorToUpdate = await GetByIdAsync(entity.Id);
+        if (authorToUpdate is null)
+        {
+            return null;
+        }
+
+        authorToUpdate.Name = entity.Name;
+        authorToUpdate.DateOfBirth = entity.DateOfBirth;
+        await _context.SaveChangesAsync();
+
+        return authorToUpdate;
+    }
+
+    public async Task<Author?> DeleteAsync(int id)
+    {
+        var authorToDelete = await GetByIdAsync(id);
+        if (authorToDelete is null)
+        {
+            return null;
+        }
+
+        _context.Authors.Remove(authorToDelete);
+        await _context.SaveChangesAsync();
+
+        return authorToDelete;
+    }
+}
---
+++ LibraryManagementApp.DataAccess/Repositories/EfBooksRepository.cs
@@ -0,0 +1,64 @@
+﻿using LibraryManagementApp.DataAccess.Contexts;
+using LibraryManagementApp.Domain.Interfaces.Repositories;
+using LibraryManagementApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementApp.DataAccess.Repositories;
+
+public class EfBooksRepository : IBooksRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public EfBooksRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Book>> GetAllAsync()
+    {
+        return await _context.Books.ToListAsync();
+    }
+
+    public async Task<Book?> GetByIdAsync(int id)
+    {
+        return await _context.Books.FindAsync(id);
+    }
+
+    public async Task<Book> CreateAsync(Book entity)
+    {
+        await _context.Books.AddAsync(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<Book?> UpdateAsync(Book entity)
+    {
+        var bookToUpdate = await GetByIdAsync(entity.Id);
+        if (bookToUpdate is null)
+        {
+            return null;
+        }
+
+        bookToUpdate.Title = entity.Title;
+        bookToUpdate.PublishedYear = entity.PublishedYear;
+        bookToUpdate.AuthorId = entity.AuthorId;
+        await _context.SaveChangesAsync();
+
+        return bookToUpdate;
+    }
+
+    public async Task<Book?> DeleteAsync(int id)
+    {
+        var bookToDelete = await GetByIdAsync(id);
+        if (bookToDelete is null)
+        {
+            return null;
+        }
+
+        _context.Books.Remove(bookToDelete);
+        await _context.SaveChangesAsync();
+
+        return bookToDelete;
+    }
+}
